e0c51bd9c442cfb28e1197c47d8a504297ac1cbc
Saahil <neon@saahild.com>
2025-09-20 17:33:30 -0400

    yesterdays worky

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,5 +1,18 @@
+import { Octokit } from "@octokit/rest";
+const OWNER = "NeonGamerBot-QK";
+const REPO = "blog";
+const BASE_BRANCH = "main";
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
 export default function POST(req: Request) {
+  // 1. grab the body and valideate auth or smt
+  // 2. reply email?!
+  // 3. p
   return new Response(JSON.stringify({ name: "Email API " + Date.now() }), {
     headers: { "Content-Type": "application/json" },
   });
+}
+
+function createPR(title, description, is_draft, auto_merge_pr, blog_content, cover) {
+
 }
