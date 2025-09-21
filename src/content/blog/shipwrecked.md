327001f596ca1f4185b61eeb3c27e523b8e49f74
Neon <neon@saahild.com>
2025-09-21 19:28:22 -0400

    a

--- src/content/blog/shipwrecked.md
+++ src/content/blog/shipwrecked.md
@@ -19,11 +19,16 @@ little showcase of my 4 main projects
 
 ### My site
 
-I had a site and i had felt like redoing it over the summer so i re did it and logged all the hours. You can see the finished product [here](https://saahild.com)
+I had a site and i had felt like redoing it over the summer so i re did it and logged all the hours. You can see the finished product [here](https://saahild.com) and see the source code [here](https://github.com/NeonGamerBot-QK/saahild.com)
 
 ### Signal app
 
+Have you ever wanted to use the signal desktop but on the web?
+Well i worked on a project for doing that! it didnt really work well and its a major wip but one day it will get finished!
+You can find the source code [here](https://github.com/NeonGamerBot-QK/signal-app)
+
 ### Fintrack
+Fin track was a tool which was also a major wip which would pull data from your github.
 
 ### Telegram HWF
 
--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -8,7 +8,9 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 export default function POST(req: Request) {
   // 1. grab the body and valideate auth or smt
   // 2. reply email?!
-  // 3. p
+  // 3. create pr #sigma
+  // TODO: finsih this up later, i need to grind hours rn
+  console.log(req.body)
   return new Response(JSON.stringify({ name: "Email API " + Date.now() }), {
     headers: { "Content-Type": "application/json" },
   });
@@ -21,6 +23,7 @@ async function createPR(
   auto_merge_pr,
   blog_content,
   cover,
+  coverData,
   date,
   flags,
 ) {
@@ -44,19 +47,18 @@ async function createPR(
   await octokit.repos.createOrUpdateFileContents({
     owner: OWNER,
     repo: REPO,
-    // adding the date just incase i
     path: `public/images/${coverName}.${coverExt}`,
     // co author neon@saahild.com
     message: `Add cover \n\nCo-Authored-by: Neon <git@saahild.com>`,
     content: Buffer.from(
-      `--\n# Created via email automation\n  title: "${title}"\n  description: "${description}"  \n  date: ${date.toISOString().split("T")[0] || new Date().toISOString().split("T")[0]}  \n  tags: ${JSON.stringify(["created-via-zeon", "created-via-email", ...(flags || [])])} \n  cover: "/images/${coverName}.${coverExt}"\n  email: true\n  zeon: true  \n---\n${blog_content}`,
+      coverData,
     ).toString("base64"),
     branch: branchName,
   });
   await octokit.repos.createOrUpdateFileContents({
     owner: OWNER,
     repo: REPO,
-    // adding the date just incase i
+    // adding the date just incase the blog exists already
     path: `src/content/blog/${title.replace(/\s+/g, "-").toLowerCase()}--${Date.now()}.md`,
     // co author neon@saahild.com
     message: `Add ${title}\n${description}\n\nCo-Authored-by: Neon <git@saahild.com>`,
@@ -65,4 +67,25 @@ async function createPR(
     ).toString("base64"),
     branch: branchName,
   });
+
+
+  // 4. Open a PR
+  const { data: pr } = await octokit.pulls.create({
+    owner: OWNER,
+    repo: REPO,
+    title: `feat(blog): ${title}`,
+    head: branchName,
+    base: BASE_BRANCH,
+    draft: is_draft,
+    body: `New blog post!here is the metadata::\n\n\`\`\`md\n--\n# Created via email automation\n  title: "${title}"\n  description: "${description}"  \n  date: ${date.toISOString().split("T")[0] || new Date().toISOString().split("T")[0]}  \n  tags: ${JSON.stringify(["created-via-zeon", "created-via-email", ...(flags || [])])} \n  cover: "/images/${coverName}.${coverExt}"\n  email: true\n  zeon: true  \n---\`\`\``,
+  });
+  if (!is_draft && auto_merge_pr) {
+    const { data: merge } = await octokit.pulls.merge({
+      owner: OWNER,
+      repo: REPO,
+      pull_number: pr.number,
+      merge_method: "squash", // can be "merge" | "squash" | "rebase"
+    });
+  }
+  return pr.html_url;
 }
